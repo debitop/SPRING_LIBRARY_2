7cac13bb031710b8db6d0724e6994f5925e35786
Aleksandr Kochkurkin <mailto:89636617290@mail.ru>
2018-09-30 13:59:05 +0200

    commit

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import repository.StudentRepository;
+
+@Controller
+@RequestMapping(value = "/student")
 public class StudentController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+
 }
